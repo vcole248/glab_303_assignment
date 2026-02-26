f9ecb134dd81b6c5f717e8b0a4ade66885f45eb4
vcole248 <william.coleman@peopleshores.com>
2026-02-26 12:01:05 -0500

    first commit

---
+++ src/Glab303_2_1/LogicalOperatorsDemo.java
@@ -0,0 +1,17 @@
+package Glab303_2_1;
+
+
+public class LogicalOperatorsDemo {
+    public static void main(String[] args)
+    {
+        boolean x = true;
+        boolean y = false;
+        System.out.println("x & y : " + (x & y));
+        System.out.println("x && y : " + (x && y));
+        System.out.println("x | y : " + (x | y));
+        System.out.println("x || y: " + (x || y));
+        System.out.println("x ^ y : " + (x ^ y));
+        System.out.println("!x : " + (!x));
+    }
+}
+
